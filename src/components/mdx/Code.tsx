6fa079147de4f10f0c1915afc87243cc96feb624
Artem Zakharchenko <kettanaito@gmail.com>
2020-06-07 12:31:58 +0200

    Code: Decreases the font-size

--- src/components/mdx/Code.tsx
+++ src/components/mdx/Code.tsx
@@ -3,13 +3,29 @@ import styled from 'styled-components'
 import { Code as ReactCdxCode } from 'react-cdx'
 import CodeTheme from 'prism-react-renderer/themes/nightOwlLight'
 import { CopyButton } from '../CopyButton'
+import { query, useResponsiveQuery, useResponsiveValue } from 'atomic-layout'
 
 const StyledCode = styled(ReactCdxCode)`
   overflow: auto;
+  font-size: 90%;
 `
 
 const Container = styled.div`
   position: relative;
+`
+
+const CopyCodeButton = styled(CopyButton)`
+  margin-top: -1rem;
+  width: 100%;
+
+  @media ${query({ from: 'md' })} {
+    margin: 0;
+    width: initial;
+
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
 `
 
 interface CodeProps {
@@ -33,6 +49,15 @@ export const Code: React.FC<CodeProps> = ({
   showLineNumbers = false,
   focusedLines,
 }) => {
+  const shouldShowCopyText = useResponsiveValue(
+    {
+      xs: true,
+      sm: true,
+      md: true,
+    },
+    false,
+  )
+
   return (
     <StyledCode
       code={children}
@@ -46,7 +71,14 @@ export const Code: React.FC<CodeProps> = ({
       {({ Preview, copyToClipboard }) => (
         <Container>
           <Preview />
-          {copyable && <CopyButton onClick={copyToClipboard} />}
+          {copyable && (
+            <CopyCodeButton
+              showText={shouldShowCopyText}
+              onClick={copyToClipboard}
+            >
+              Copy this example
+            </CopyCodeButton>
+          )}
         </Container>
       )}
     </StyledCode>
