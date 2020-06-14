61a6f78d4ba61cb1f914f3ff09e6c934162b362f
Artem Zakharchenko <kettanaito@gmail.com>
2020-06-14 13:58:13 +0200

    Homepage: Adds "Teachers" diagram

--- src/sections/Workshops.tsx
+++ src/sections/Workshops.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Box } from 'atomic-layout'
 
 import { TextLead } from '../components/TextLead'
@@ -7,11 +8,80 @@ import { Text } from '../components/Text'
 import { Section, SectionContent } from '../components/Section'
 import { Heading } from '../components/Heading'
 
+import { ReactComponent as TeachersDiagram } from '../images/teachers-diagram.svg'
+
+const AnimatedDiagram = styled(TeachersDiagram)`
+  #progress-left,
+  #progress-top,
+  #progress-bottom,
+  #progress-right {
+    animation: strokeOffset 30s linear infinite;
+  }
+
+  #teacher-circle-beat {
+    animation: pulseAndFade 5s ease infinite;
+    transform-origin: 32px 32px;
+  }
+
+  #middle-fill,
+  #right-fill,
+  #left-fill {
+    mask: url('#mask-4');
+    transform-origin: 32px 32px;
+    animation: fillUp 5s ease infinite backwards;
+  }
+
+  #left-fill {
+    animation-duration: 6.5s;
+  }
+
+  #right-fill {
+    animation-duration: 7.5s;
+  }
+
+  @keyframes pulseAndFade {
+    0%,
+    50% {
+      opacity: 0.5;
+      transform: scale(1);
+    }
+
+    100% {
+      opacity: 0;
+      transform: scale(1.5);
+    }
+  }
+
+  @keyframes fillUp {
+    0% {
+      y: 64;
+    }
+    50% {
+      y: 0;
+      mask: url('#mask-4');
+      transform: scale(1);
+      rx: 0;
+    }
+    51% {
+      opacity: 1;
+      transform: scale(1);
+      rx: 50%;
+      mask: unset;
+    }
+    100% {
+      mask: unset;
+      opacity: 0;
+      rx: 50%;
+      transform: scale(1.5);
+    }
+  }
+`
+
 export const Workshops = () => {
   return (
     <Section>
       <Box orderLg={1}>
-        <p>Some image</p>
+        <AnimatedDiagram />
       </Box>
       <SectionContent>
         <Heading level={2} marginBottom={8} align="center" alignLg="start">
