1e35cf8987b8dcbda30910fa1930b6c4aa72776b
Artem Zakharchenko <kettanaito@gmail.com>
2020-06-16 14:41:40 +0200

    Breadcrumbs: Makes breadcrumbs structure SEO-friendly

--- src/templates/docs/components/Breadcrumbs.tsx
+++ src/templates/docs/components/Breadcrumbs.tsx
@@ -53,14 +53,37 @@ interface BreadcrumbsProps {
 export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ items }) => {
   return (
     <ScrollableContainer>
-      <Box as={StyledList} flex alignItems="center" marginBottom={16}>
+      <Box
+        as={StyledList}
+        flex
+        alignItems="center"
+        marginBottom={16}
+        itemScope={true}
+        itemType="https://schema.org/BreadcrumbList"
+      >
         {items.map((item, index) => (
-          <li key={index}>
+          <li
+            key={index}
+            itemScope={true}
+            itemType="https://schema.org/ListItem"
+            itemProp="itemListElement"
+          >
             {index === items.length - 1 ? (
-              <span className="current-page">{item.title}</span>
+              <span className="current-page" itemProp="name">
+                {item.title}
+              </span>
             ) : (
-              <Link to={item.url}>{item.title}</Link>
+              <Link
+                to={item.url}
+                itemScope={true}
+                itemType="https://schema.org/Thing"
+                itemProp="item"
+                itemID={item.url}
+              >
+                <span itemProp="name">{item.title}</span>
+              </Link>
             )}
+            <meta itemProp="position" content={index.toString()} />
           </li>
         ))}
       </Box>
