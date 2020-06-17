cbe8a27c2aa231b4a7a78ad5c6ad1bd0bea9fe50
Artem Zakharchenko <kettanaito@gmail.com>
2020-06-17 20:23:47 +0200

    SEO: Removes "user-scalable" meta value

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -90,7 +90,7 @@ const SEO: React.FC<Props> = ({
     >
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, shrink-to-fit=no"
       />
 
       {/* Description */}
