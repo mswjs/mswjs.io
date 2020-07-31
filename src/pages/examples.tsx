95d8548ee85a1895a8768ab3a9f0fb95f082920e
Artem Zakharchenko <kettanaito@gmail.com>
2020-07-31 11:06:51 +0200

    Examples: Retouches the layout

--- src/pages/examples.tsx
+++ src/pages/examples.tsx
@@ -4,18 +4,20 @@ import { Composition } from 'atomic-layout'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { Grid } from '../components/Grid'
-import { ExampleItem } from '../components/exampleItem'
+import { ExampleItem } from '../components/ExampleItem'
+import { Text } from '../components/Text'
 import { TextLead } from '../components/TextLead'
 import { ReactComponent as GraphQLLogo } from '../images/graphql-logo.svg'
 import { ReactComponent as JavaScriptIcon } from '../images/logos/javascript-2.svg'
 import { ReactComponent as StorybookIcon } from '../images/logos/storybook.svg'
+import reduxSagaLogo from '../images/logos/redux-saga.png'
 import { Heading } from '../components/Heading'
 
 const ExampleList: React.FC = ({ children }) => {
   return (
     <Composition
       gap={32}
-      gapMd={64}
+      gapLg={64}
       templateColsMd="repeat(2, 1fr)"
       templateColsLg="repeat(3, 1fr)"
     >
@@ -36,23 +38,30 @@ const Examples = () => {
       <Grid paddingVertical={64} paddingVerticalMd={80}>
         <Heading>Examples</Heading>
         <TextLead>
-          Browse through the collection of Mock Service Worker usage examples
-          with various libraries and frameworks.
+          Learn by inspecting how Mock Service Worker integrates with your
+          favorite tools.
         </TextLead>
+        <hr />
         <Composition gap={80} marginTop={80}>
           <section>
             <Heading level={2}>API types</Heading>
+            <Text>
+              Each example in this section contains a complete showcase of Mock
+              Service Worker usage in development, unit, and E2E tests. A great
+              place to start with API mocking when all you know is the API type
+              your application uses.
+            </Text>
             <ExampleList>
               <ExampleItem
-                textLide="REST API"
-                text="Learn how to mock a RESTful API in a React application."
+                title="REST API"
+                description="Learn how to mock a RESTful API in a React application."
                 logo={JavaScriptIcon}
                 fill="red"
                 url="https://github.com/mswjs/examples/tree/master/examples/rest-react"
               />
               <ExampleItem
-                textLide="GraphQL API (Apollo)"
-                text="Learn how to mock a GraphQL API in a React application using Apollo."
+                title="GraphQL API"
+                description="Learn how to mock a GraphQL API in a React application using Apollo."
                 logo={GraphQLLogo}
                 fill="#E535AB"
                 url="https://github.com/mswjs/examples/tree/master/examples/graphql-react-apollo"
@@ -61,15 +70,26 @@ const Examples = () => {
           </section>
           <section>
             <Heading level={2}>Libraries & Frameworks</Heading>
-            <ExampleList>
+            <Text>
+              Examples below are strictly targeted on the integration of Mock
+              Service Worker with other tools.
+            </Text>
+            <Composition gap={24}>
               <ExampleItem
-                textLide="Storybook"
-                text="Integrate API mocking for your stories in Storybook."
+                mode="minimal"
+                title="Storybook"
                 logo={StorybookIcon}
                 fill="#ff4785"
                 url="https://github.com/mswjs/examples/tree/master/examples/with-storybook"
               />
-            </ExampleList>
+              <ExampleItem
+                mode="minimal"
+                title="Redux Saga"
+                logo={(props) => <img {...props} src={reduxSagaLogo} />}
+                fill="#ff4785"
+                url="https://github.com/mswjs/examples/tree/master/examples/with-redux-saga"
+              />
+            </Composition>
           </section>
         </Composition>
       </Grid>
