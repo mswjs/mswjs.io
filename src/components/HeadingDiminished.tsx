9bd6b93a3542796412bdb5fc4600ad5150588737
Artem Zakharchenko <kettanaito@gmail.com>
2020-08-03 11:31:52 +0200

    Adds the companies to the homepage

--- src/components/HeadingDiminished.tsx
+++ src/components/HeadingDiminished.tsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components'
+import { Heading } from './Heading'
 
-export const HeadingDiminished = styled.h4`
+export const HeadingDiminished = styled(Heading)`
   font-size: 0.8rem;
   letter-spacing: 0.2ch;
   text-transform: uppercase;
 `
+
+HeadingDiminished.defaultProps = {
+  level: 4,
+}
