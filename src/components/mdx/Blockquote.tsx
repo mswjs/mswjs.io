76d5633d0d7ca43771f733febe4fa3532d8c8dbb
Artem Zakharchenko <kettanaito@gmail.com>
2020-08-28 17:37:44 +0200

    Adds minor stylistic improvements

--- src/components/mdx/Blockquote.tsx
+++ src/components/mdx/Blockquote.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Blockquote = styled.blockquote`
   margin-left: 0;
   padding: 0.5rem;
-  padding-left: 1.5rem;
+  padding-left: 1rem;
   border-left: 3px solid var(--color-gray-light);
   font-size: 90%;
   font-weight: 500;
