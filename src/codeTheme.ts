4d29bc6da57720c7f2d33d3f2570a7676944c4ea
Jens Meindertsma <jens@jensmeindertsma.com>
2020-12-12 12:19:20 +0100

    Delete everything

--- src/codeTheme.ts
+++
@@ -1,9 +0,0 @@
-import { PrismTheme } from 'prism-react-renderer'
-import CodeTheme from 'prism-react-renderer/themes/nightOwl'
-
-export const theme: PrismTheme = Object.assign({}, CodeTheme as any, {
-  plain: {
-    color: '#d6deeb',
-    backgroundColor: 'var(--color-black)',
-  },
-})
--- src/components/TextSmall.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-import { TextProps, Text } from './Text'
-
-export const TextSmall = styled(Text)<TextProps>`
-  font-size: 0.9rem;
-  line-height: 1.4;
-`
