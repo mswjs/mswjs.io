361075ca4b7dc7ccf40c12bb050eaeaf9657fd79
Artem Zakharchenko <kettanaito@gmail.com>
2023-10-21 14:36:46 +0200

    hide egghead course components for now

--- src/components/react/eggheadCourseBanner.tsx
+++ src/components/react/eggheadCourseBanner.tsx
@@ -3,6 +3,10 @@ import courseCover from '../../images/egghead-msw-course.png'
 import { EGGHEAD_COURSE_URL } from '../../consts'
 
 export function EggheadCourseBanner() {
+  if (import.meta.env.PUBLIC_HIDE_EGGHEAD_COURSE === 'true') {
+    return <></>
+  }
+
   return (
     <LinkExternal
       href={EGGHEAD_COURSE_URL}
