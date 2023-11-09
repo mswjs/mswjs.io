d3d8baa535b86e32dccecba571448f3343e7b5f3
Artem Zakharchenko <kettanaito@gmail.com>
2023-11-09 11:36:03 +0100

    add react native integration

--- src/components/react/warning.tsx
+++ src/components/react/warning.tsx
@@ -4,9 +4,8 @@ export function Warning({ children }: { children: ReactNode }): JSX.Element {
   return (
     <div
       role="alert"
-      className="flex items-start gap-2 px-4 py-2.5 my-5 text-yellow-500 bg-yellow-900 bg-opacity-10 font-medium leading-6 border border-yellow-800 rounded-lg"
+      className="px-4 py-2.5 my-5 text-yellow-500 bg-yellow-900 bg-opacity-10 font-medium leading-6 border border-yellow-800 rounded-lg"
     >
-      {/* <ExclamationIcon className="flex-shrink-0 w-4 mt-1 text-yellow-500" /> */}
       {children}
     </div>
   )
