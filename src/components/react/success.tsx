82e5b6135c66e146c80731054749752d8afc1d71
Artem Zakharchenko <kettanaito@gmail.com>
2023-11-09 11:37:43 +0100

    add react native integration (#311)

--- src/components/react/success.tsx
+++ src/components/react/success.tsx
@@ -4,7 +4,7 @@ export function Success({ children }: { children: ReactNode }): JSX.Element {
   return (
     <div
       role="alert"
-      className="flex items-start gap-2 px-4 py-2.5 my-5 font-medium text-green-600 bg-green-900 bg-opacity-10 border border-green-800 rounded-lg"
+      className="px-4 py-2.5 my-5 font-medium text-green-600 bg-green-900 bg-opacity-10 border border-green-800 rounded-lg"
     >
       {children}
     </div>
