e13ab4c81d1117b9b61507b4febc49baef190dc3
Artem Zakharchenko <kettanaito@gmail.com>
2024-02-21 10:45:08 +0100

    add describing websocket api page

--- src/components/react/action.tsx
+++ src/components/react/action.tsx
@@ -3,9 +3,9 @@ import { ArrowRightCircleIcon } from '@heroicons/react/24/solid'
 
 export function Action({ children }: { children: ReactNode }) {
   return (
-    <p className="text-orange font-semibold">
-      <ArrowRightCircleIcon className="inline w-5 -mt-[0.25ch] mr-[0.5ch] align-middle" />
+    <div className="action flex items-start gap-0.5 text-orange font-semibold leading-6">
+      <ArrowRightCircleIcon className="inline w-5 mt-[2px] mr-[0.5ch] align-middle" />
       {children}
-    </p>
+    </div>
   )
 }
