da01dfdc4f3c1a4f7464a1ba6bc8ab27d07e7cf8
Artem Zakharchenko <kettanaito@gmail.com>
2024-06-03 18:32:23 +0200

    add some docs

---
+++ websites/source.mswjs.io/src/content/buildDocsNavTree.ts
@@ -0,0 +1,20 @@
+import type { DocsFrontmatter } from '@mswjs/shared/collections/docs'
+import {
+  createNavTreeBuilder,
+  type DocsItem,
+} from '@mswjs/shared/utils/buildNavTree'
+
+export function buildDocsNavTree(
+  pages: Array<MDXInstance<DocsFrontmatter>>
+): Array<DocsItem> {
+  const builder = createNavTreeBuilder(pages)
+
+  return [
+    ...builder.get('*.mdx'),
+    {
+      kind: 'group',
+      title: 'API',
+      children: builder.get('api/**/*.mdx'),
+    },
+  ]
+}
