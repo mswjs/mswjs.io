fa37acfd51cddf04a20d9bcf215d1c966c997306
Artem Zakharchenko <kettanaito@gmail.com>
2024-06-07 13:30:13 +0200

    improve source docs

--- websites/source.mswjs.io/tailwind.config.cjs
+++ websites/source.mswjs.io/tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [require('@mswjs/shared/tailwind.config.cjs')],
-  content: [`../shared/**/*.{astro,ts,tsx}`, `./src/**/*.{astro,ts,tsx}`],
+  content: [`../shared/**/*.{astro,ts,tsx}`, `./src/**/*.{astro,ts,tsx,mdx}`],
   theme: {
     extend: {
       colors: {
