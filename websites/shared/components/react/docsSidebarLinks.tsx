fd469c4d136699924d674418244aab1152e20265
Artem Zakharchenko <kettanaito@gmail.com>
2024-06-25 16:41:05 +0200

    make docs sidebar links dynamic

--- websites/shared/components/react/docsSidebarLinks.tsx
+++ websites/shared/components/react/docsSidebarLinks.tsx
@@ -1,9 +1,15 @@
+import * as React from 'react'
 import { NewspaperIcon } from '@heroicons/react/24/solid'
 import { DiscordIcon } from './icons/discord'
 import { GitHubIcon } from './icons/github'
 import { DISCORD_URL } from '../../consts'
 
-export function DocsSidebarLinks() {
+interface DocsSidebarLinksProps {
+  gitHubUrl: string
+  blogUrl: string
+}
+
+export function DocsSidebarLinks(props: DocsSidebarLinksProps) {
   return (
     <section>
       <h4 className="text-xs mb-2 font-bold tracking-widest text-white uppercase">
@@ -12,7 +18,7 @@ export function DocsSidebarLinks() {
       <ul>
         <li>
           <a
-            href="https://github.com/mswjs/msw"
+            href={props.gitHubUrl}
             className="py-1.5 inline-flex items-center gap-1.5 leading-5 text-neutral-400 hover:text-white"
             target="_blank"
             rel="noopener noreferrer"
@@ -34,7 +40,7 @@ export function DocsSidebarLinks() {
         </li>
         <li>
           <a
-            href="/blog"
+            href={props.blogUrl}
             className="py-1.5 inline-flex items-center gap-1.5 leading-5 text-neutral-400 hover:text-white"
             target="_blank"
             ref="noopener noreferrer"
