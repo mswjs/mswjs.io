5a14a64289d0c4d5540d8186172f572296891d4b
Artem Zakharchenko <kettanaito@gmail.com>
2024-06-26 17:55:53 +0200

    add ecosystem page to mswjs.io

--- websites/shared/components/react/iconBlock.tsx
+++ websites/shared/components/react/iconBlock.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export function IconBlock(props: Props): JSX.Element {
   return (
-    <div className="inline-block border-2 border-neutral-800 bg-neutral-900 rounded-xl p-5 w-24 shadow-xl">
+    <div className="inline-block flex-shrink-0 border-2 border-neutral-800 bg-neutral-900 rounded-xl p-5 w-24 shadow-xl">
       <img src={props.imageUrl} alt={props.alt} className="flex select-none" />
     </div>
   )
