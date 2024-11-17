ce4ee5359704845eea4cc4a7ede74f762293d565
Artem Zakharchenko <kettanaito@gmail.com>
2024-11-17 18:35:05 +0100

    add thubmnail, query-styled egghead course component

--- websites/mswjs.io/src/components/react/eggheadCourseBanner.tsx
+++ websites/mswjs.io/src/components/react/eggheadCourseBanner.tsx
@@ -1,6 +1,5 @@
 import { LinkExternal } from '@mswjs/shared/components/react/linkExternal'
 import courseCover from '../../images/egghead-msw-course.png'
-import { EGGHEAD_COURSE_URL } from '../../consts'
 
 export function EggheadCourseBanner({
   courseUrl,
@@ -12,18 +11,20 @@ export function EggheadCourseBanner({
   description: React.ReactNode
 }) {
   return (
-    <LinkExternal
-      href={courseUrl}
-      className="relative group my-10 px-5 py-5 min-h-[125px] grid grid-cols-[150px_1fr] gap-8 items-center bg-neutral-800 bg-opacity-40 border border-neutral-800 rounded-lg no-underline hover:bg-opacity-70"
-    >
-      <img
-        src={courseCover.src}
-        className="absolute m-0 top-0 left-3 w-[170px] transition-transform duration-500 -translate-y-[25px] group-hover:-translate-y-[35px]"
-      />
-      <div className="col-start-2">
-        <h4 className="mt-0">{title}</h4>
-        <p className="mb-0 text-neutral-400 leading-6">{description}</p>
-      </div>
-    </LinkExternal>
+    <div className="@container">
+      <LinkExternal
+        href={courseUrl}
+        className="relative group my-10 px-5 py-5 min-h-[125px] grid grid-rows-[160px,1fr] @lg:grid-rows-1 @lg:grid-cols-[150px_1fr] gap-5 @lg:gap-8 items-center bg-neutral-800 bg-opacity-40 border border-neutral-800 rounded-lg no-underline hover:bg-opacity-70"
+      >
+        <img
+          src={courseCover.src}
+          className="mx-auto w-[200px] -translate-y-5 group-hover:-translate-y-[35px] transition-transform duration-500 @lg:absolute @lg:top-0 @lg:w-[170px] @lg:-translate-y-[25px] @lg:left-3 @lg:m-0"
+        />
+        <div className="@lg:col-start-2">
+          <h4 className="mt-0">{title}</h4>
+          <p className="mb-0 text-neutral-400 leading-6">{description}</p>
+        </div>
+      </LinkExternal>
+    </div>
   )
 }
